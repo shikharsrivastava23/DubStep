60fbeca15098e03b909c3e6e34330bd9b3fe650a
LiteLife <arnabmu1999@gmail.com>
2020-04-02 01:15:20 +0530

    Splash Screen made

---
+++ app/src/main/java/com/example/dubstep/Splash_Screen.java
@@ -0,0 +1,35 @@
+package com.example.dubstep;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class Splash_Screen extends AppCompatActivity {
+
+    TextView dubstep, dabba;
+    ImageView logo;
+
+    private static int SPLASH_SCREEN = 4000; //4 Ssecs
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash__screen);
+
+        dubstep = findViewById(R.id.dubstepText);
+        dabba = findViewById(R.id.dabbaText);
+        logo = findViewById(R.id.logoImage);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(Splash_Screen.this, LoginActivity.class));
+                finish();
+            }
+        },SPLASH_SCREEN);
+    }
+}
