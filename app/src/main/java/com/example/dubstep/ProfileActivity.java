3f94ae241bae14071d6b66848492464fc8a01ad2
LiteLife <arnabmu1999@gmail.com>
2020-04-12 03:36:01 +0530

    Profile tab made

--- app/src/main/java/com/example/dubstep/ProfileActivity.java
+++ app/src/main/java/com/example/dubstep/ProfileActivity.java
@@ -10,17 +10,37 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class ProfileActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private Toolbar toolbar;
     private DrawerLayout drawerLayout;
     private NavigationView navigationView;
-    private TextInputEditText mUsername;
+    private EditText mUsername;
+    private EditText mFullName;
+    private EditText mMobileNumber;
+    private EditText mEmail;
+    private Button UpdateButton;
+    private GoogleSignInClient mGoogleSignInClient;
+    private FirebaseAuth firebaseAuth;
+    DatabaseReference databaseReference;
 
 
     @Override
@@ -46,7 +66,99 @@ public class ProfileActivity extends AppCompatActivity implements NavigationView
         navigationView.setNavigationItemSelectedListener(this);
         navigationView.setCheckedItem(R.id.profile_nav);
 
-        mUsername = findViewById(R.id.UsernameTextInput);
+        mUsername = findViewById(R.id.mUsername);
+        mFullName = findViewById(R.id.mFullName);
+        mMobileNumber = findViewById(R.id.mMobileNumber);
+        mEmail = findViewById(R.id.mEmail);
+        UpdateButton = findViewById(R.id.UpdateButton);
+
+        databaseReference = FirebaseDatabase.getInstance().getReference("user");
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        FirebaseDatabase.getInstance().getReference("user")
+                .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                .addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        String Username = dataSnapshot.child("Username").getValue().toString();
+                        String FullName = dataSnapshot.child("fullName").getValue().toString();
+                        String Email = dataSnapshot.child("Email").getValue().toString();
+                        String PhoneNumber = dataSnapshot.child("PhoneNumber").getValue().toString();
+                        mUsername.setText(Username);
+                        mFullName.setText(FullName);
+                        mMobileNumber.setText(PhoneNumber);
+                        mEmail.setText(Email);
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+
+
+
+        UpdateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                FirebaseDatabase.getInstance().getReference("user")
+                        .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                        .addValueEventListener(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                String Username = dataSnapshot.child("Username").getValue().toString();
+                                String FullName = dataSnapshot.child("fullName").getValue().toString();
+                                String Email = dataSnapshot.child("Email").getValue().toString();
+                                String PhoneNumber = dataSnapshot.child("PhoneNumber").getValue().toString();
+
+                                final String User_Name = mUsername.getText().toString();
+                                final String Full_Name = mFullName.getText().toString();
+                                final String E_mail = mEmail.getText().toString();
+                                final String Phone_Number = mMobileNumber.getText().toString();
+
+                                if (!Full_Name.equals(FullName)) {
+                                    FirebaseDatabase.getInstance().getReference("user")
+                                            .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                                            .child("fullName").setValue(Full_Name);
+                                    mFullName.setText(Full_Name);
+                                }
+                                if (!User_Name.equals(Username)) {
+                                    FirebaseDatabase.getInstance().getReference("user")
+                                            .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                                            .child("Username").setValue(User_Name);
+                                    mUsername.setText(User_Name);
+                                }
+                                if (!E_mail.equals(Email)) {
+                                    FirebaseDatabase.getInstance().getReference("user")
+                                            .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                                            .child("Email").setValue(E_mail);
+                                    mEmail.setText(E_mail);
+                                }
+                                if (!Phone_Number.equals(PhoneNumber)){
+                                    FirebaseDatabase.getInstance().getReference("user")
+                                            .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                                            .child("PhoneNumber").setValue(Phone_Number);
+                                    mMobileNumber.setText(Phone_Number);
+                                }
+                            }
+
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                            }
+                        });
+                Toast.makeText(ProfileActivity.this,  "Updated",Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 
     @Override
@@ -61,6 +173,7 @@ public class ProfileActivity extends AppCompatActivity implements NavigationView
                 break;
             case R.id.log_out:
                 FirebaseAuth.getInstance().signOut();
+                mGoogleSignInClient.signOut();
                 startActivity(new Intent(ProfileActivity.this, LoginActivity.class));
                 finish();
         }
