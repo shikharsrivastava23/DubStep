3b82e3c0a0f7bc85ba3a4097e801112fd5aad284
Shikhar <f20170794@hyderabad.bits-pilani.ac.in>
2020-04-12 01:38:01 +0530

    Main Activity redirects to Rider Main Activity according to user role.

--- app/src/main/java/com/example/dubstep/MainActivity.java
+++ app/src/main/java/com/example/dubstep/MainActivity.java
@@ -9,26 +9,36 @@ import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import com.google.android.material.button.MaterialButton;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     Button btn_logout;
     FirebaseAuth firebaseAuth;
     private FirebaseAuth.AuthStateListener mAuthStateListener;
+    private DatabaseReference userref;
 
     private Toolbar toolbar;
     private DrawerLayout drawerLayout;
     private NavigationView navigationView;
 
     private FloatingActionButton mCartButton;
+    user current_user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,6 +58,25 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 }
             }
         };
+
+
+        userref = FirebaseDatabase.getInstance().getReference("user").child(firebaseAuth.getCurrentUser().getUid().toString());
+        userref.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                current_user = dataSnapshot.getValue(user.class);
+
+                if(current_user.Role.equals("Rider")){
+                    gotorider();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
 
         toolbar = findViewById(R.id.main_toolbar);
         drawerLayout = findViewById(R.id.drawer_layout);
@@ -80,6 +109,12 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
 
 
+    }
+
+    private void gotorider() {
+        Intent intent = new Intent(MainActivity.this,RiderMainActivity.class);
+        startActivity(intent);
+        finish();
     }
 
     @Override
