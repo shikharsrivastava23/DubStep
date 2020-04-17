bc6ac052e59f49cc12bc7270da64862a23659ae6
LiteLife <arnabmu1999@gmail.com>
2020-04-17 11:45:36 +0530

    Login changes based on rider/customer, splash screen changed to 2.5 secs

--- app/src/main/java/com/example/dubstep/TempActivity.java
+++ app/src/main/java/com/example/dubstep/TempActivity.java
@@ -32,7 +32,7 @@ public class TempActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_temp);
+        //setContentView(R.layout.activity_temp);
 
 
         //////////////////////////////////////////////////////////////
