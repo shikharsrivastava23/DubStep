ad169ca08b1653ec10a4b0609144099b1856e9a2
Intellegion <thunderhunter31@gmail.com>
2020-04-25 11:03:59 +0530

    Fixed location permission bug

--- app/src/main/java/com/example/dubstep/Splash_Screen.java
+++ app/src/main/java/com/example/dubstep/Splash_Screen.java
@@ -1,8 +1,12 @@
 package com.example.dubstep;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.widget.ImageView;
@@ -21,6 +25,21 @@ public class Splash_Screen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash__screen);
+
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED)
+        {
+            // No explanation needed; request the permission
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 0);
+
+
+        }
+        else {
+            // Permission has already been granted
+
+        }
 
         dubstep = findViewById(R.id.dubstepText);
         dabba = findViewById(R.id.dabbaText);
